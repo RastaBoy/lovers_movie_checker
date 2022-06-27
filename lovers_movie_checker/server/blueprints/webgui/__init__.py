55c6402c878a43d4770cc221b6feb00b59274c87
Alexander Troyan <trustyone3224@gmail.com>
2022-06-27 16:47:11 +0700

    Webgui

--- lovers_movie_checker/server/blueprints/webgui/__init__.py
+++ lovers_movie_checker/server/blueprints/webgui/__init__.py
@@ -1,3 +1,19 @@
+import os
+
 from quart import Blueprint
 
-webgui_blueprint = Blueprint('webgui_blueprint', __name__) # Нужно будет добавить статику
+
+webgui_blueprint = Blueprint(
+    'webgui_blueprint',
+     __name__,
+    static_folder=os.path.join(os.getcwd(), 'static')
+)
+
+
+@webgui_blueprint.route('/', defaults={'path': ''})
+@webgui_blueprint.route('/<path:path>')
+async def index(path):
+    if not ('.' in path):
+        return await webgui_blueprint.send_static_file('index.html')
+    else:
+        return await webgui_blueprint.send_static_file(path)
